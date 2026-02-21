3185d7f2b799ddcc92d6afcd4cdcc4b13d8e1ce4
mukuhaben <254engineerben@example.com>
2026-02-21 08:52:19 +0300

    #

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -126,33 +126,45 @@ useEffect(() => {
   >
     mukuhabenson@gmail.com
   </a>{" "}
-  /{" "}
-  <a
-    href="mailto:254engineerben@gmail.com"
-    style={{ color: "#cbd5e1", textDecoration: "none" }}
-  >
-    254engineerben@gmail.com
-  </a>
+ 
   <br />
 
-  
   <a
     href="https://www.linkedin.com/in/ngatiaben"
     target="_blank"
     rel="noopener noreferrer"
     style={{ color: "#cbd5e1", textDecoration: "none" }}
   >
+     {/* LinkedIn SVG */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path d="M4.98 3.5C4.98 4.88 3.87 6 2.49 6S0 4.88 0 3.5 1.11 1 2.49 1 4.98 2.12 4.98 3.5zM.24 8h4.5v16h-4.5V8zm7.26 0h4.32v2.19h.06c.6-1.13 2.07-2.32 4.26-2.32 4.56 0 5.4 3 5.4 6.89V24h-4.5v-7.58c0-1.81-.03-4.14-2.52-4.14-2.52 0-2.91 1.97-2.91 4.01V24h-4.5V8z"/>
+      </svg> {" "}
     www.linkedin.com/in/ngatiaben
   </a>
   <br />
-
-
+           
   <a
     href="https://github.com/mukuhaben"
     target="_blank"
     rel="noopener noreferrer"
     style={{ color: "#cbd5e1", textDecoration: "none" }}
   >
+      {/* GitHub SVG */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 .5C5.73.5.75 5.48.75 11.75c0 5.09 3.29 9.41 7.86 10.93.57.11.78-.25.78-.56 0-.27-.01-1.17-.02-2.12-3.2.7-3.88-1.54-3.88-1.54-.52-1.33-1.27-1.69-1.27-1.69-1.04-.71.08-.7.08-.7 1.15.08 1.75 1.18 1.75 1.18 1.02 1.75 2.67 1.24 3.32.95.1-.74.4-1.24.73-1.52-2.55-.29-5.24-1.28-5.24-5.7 0-1.26.45-2.3 1.18-3.11-.12-.29-.51-1.46.11-3.05 0 0 .96-.31 3.15 1.18a10.95 10.95 0 0 1 5.74 0c2.19-1.49 3.15-1.18 3.15-1.18.62 1.59.23 2.76.11 3.05.73.81 1.18 1.85 1.18 3.11 0 4.43-2.69 5.4-5.25 5.68.41.35.77 1.04.77 2.1 0 1.52-.01 2.75-.01 3.13 0 .31.21.67.79.56A11.26 11.26 0 0 0 23.25 11.75C23.25 5.48 18.27.5 12 .5z"/>
+      </svg> {" "}
     github.com/mukuhaben
   </a>
   <br />
